8cf68c45e51d62163d25f834ecbf1b28f0aa3720
bryan kennedy <bkennedy@smm.org>
2013-02-15 13:58:00 -0600

    Adding basic structure of a ChromeDriver Launcher

---
+++ browser.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument('--user-agent=""')
+options.add_argument('--kiosk')
+
+# Launch a Chrome instance with the appropriate options
+driver = webdriver.Chrome(chrome_options=options)
+
+# Visit a page
+driver.get("http://www.example.com")
+
+driver.quit()
